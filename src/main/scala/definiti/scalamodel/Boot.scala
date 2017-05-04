1b6a609dbafc7f043e827666e43a2a809459d7bf
Gaëtan Rizio <gaetan@rizio.fr>
2017-05-04 21:50:23 +0200

    Correct Boot to use `Project` type from core project
    * Simplify the Boot by using the `Project` interface from core
    * Adapt process to avoid calling core types (made private)

--- src/main/scala/definiti/scalamodel/Boot.scala
+++ src/main/scala/definiti/scalamodel/Boot.scala
@@ -3,101 +3,35 @@ package definiti.scalamodel
 import java.nio.charset.StandardCharsets
 import java.nio.file.{Files, Paths, StandardOpenOption}
 
-import definiti.core.parser.antlr.{CoreDefinitionLexer, CoreDefinitionParser, DefinitiLexer, DefinitiParser}
-import definiti.core.parser._
-import definiti.core.{ClassDefinition, ReferenceContext, Root, Verification}
-import org.antlr.v4.runtime._
+import definiti.core._
 
 import scala.collection.JavaConverters._
-import scala.collection.mutable.ListBuffer
-
-case class ErrorItem(line: Int, column: Int, msg: String)
-
-/**
- * This listener can be used to use the real logger instead of simply print to the console.
- */
-class ErrorListener extends BaseErrorListener {
-  private val errorsBuffer = ListBuffer[ErrorItem]()
-
-  override def syntaxError(recognizer: Recognizer[_, _], offendingSymbol: Object, line: Int, charPositionInLine: Int, msg: String, e: RecognitionException): Unit =
-    errorsBuffer.append(ErrorItem(line, charPositionInLine, msg))
-
-  def log(): Unit = errorsBuffer.foreach(System.err.println)
-
-  def hasError: Boolean = errorsBuffer.nonEmpty
-
-  def errors: Seq[ErrorItem] = errorsBuffer
-}
 
 object Boot extends App {
-  val source = Paths.get("src", "main", "resources", "samples", "first.def")
-  val destination = Paths.get("target", "samples", "result.scala")
-  val rootDefinitionFiles = Paths.get("src", "main", "resources", "api")
-  val definitionFiles = Files.find(rootDefinitionFiles, 1000, (path, _) => String.valueOf(path).endsWith(".definition"))
-
   try {
-    parseDefinitiFile(source.toString) match {
-      case Left(errors) => errors.foreach(System.err.println)
-      case Right(ast) =>
-        val typeBuffer = ListBuffer[ClassDefinition]()
-        val verificationBuffer = ListBuffer[Verification]()
-        definitionFiles.forEach { definitionFile =>
-          parseCoreDefinitionFile(definitionFile.toString) match {
-            case Left(errors) => errors.foreach(System.err.println)
-            case Right(classDefinitions) => classDefinitions.foreach(typeBuffer.append(_))
-          }
-        }
+    val configuration = Configuration(
+      source = Paths.get("src", "main", "resources", "samples", "first.def"),
+      core = CoreConfiguration(
+        source = Paths.get("src", "main", "resources", "api")
+      )
+    )
+    val destination = Paths.get("target", "samples", "result.scala")
 
-        verificationBuffer.append(ast.verifications: _*)
-        typeBuffer.append(ast.classDefinitions: _*)
-
-        implicit val context = ReferenceContext(typeBuffer, verificationBuffer)
-        ASTValidation.validate(ast) match {
-          case Valid =>
-            val result = ScalaASTBuilder.build(ast)
-            Files.createDirectories(destination.getParent)
-            Files.write(destination, Seq(result).asJava, StandardCharsets.UTF_8, StandardOpenOption.CREATE, StandardOpenOption.TRUNCATE_EXISTING)
-          case Invalid(errors) =>
-            errors.foreach(System.err.println)
-        }
+    val project = new Project(configuration)
+    project.load() match {
+      case Left(errors) =>
+        errors.foreach(System.err.println)
+      case Right(projectResult) =>
+        val root = projectResult.root
+        implicit val contexte = projectResult.context
+        val result = ScalaASTBuilder.build(root)
+        Files.createDirectories(destination.getParent)
+        Files.write(destination, Seq(result).asJava, StandardCharsets.UTF_8, StandardOpenOption.CREATE, StandardOpenOption.TRUNCATE_EXISTING)
     }
-    println("done")
   } catch {
     // In some cases, an Exception is thrown because the parser do not recognize an expression and crash its tree.
     // Did not happened with a successful syntax yet.
     case e: Exception =>
       e.printStackTrace()
-  }
-
-  def parseDefinitiFile(source: String): Either[Seq[ErrorItem], Root] = {
-    val in = CharStreams.fromFileName(source)
-    val lexer = new DefinitiLexer(in)
-    val tokens = new CommonTokenStream(lexer)
-    val parser = new DefinitiParser(tokens)
-    val errorListener = new ErrorListener()
-    parser.removeErrorListeners()
-    parser.addErrorListener(errorListener)
-    val result: DefinitiParser.DefinitiContext = parser.definiti()
-    if (errorListener.hasError) {
-      Left(errorListener.errors)
-    } else {
-      Right(DefinitiASTParser.definitiContextToAST(result))
-    }
-  }
-
-  def parseCoreDefinitionFile(source: String): Either[Seq[ErrorItem], Seq[ClassDefinition]] = {
-    val in = CharStreams.fromFileName(source)
-    val lexer = new CoreDefinitionLexer(in)
-    val tokens = new CommonTokenStream(lexer)
-    val parser = new CoreDefinitionParser(tokens)
-    val errorListener = new ErrorListener()
-    parser.removeErrorListeners()
-    parser.addErrorListener(errorListener)
-    val result: CoreDefinitionParser.CoreDefinitionContext = parser.coreDefinition()
-    if (errorListener.hasError) {
-      Left(errorListener.errors)
-    } else {
-      Right(CoreDefinitionASTParser.definitionContextToAST(result))
-    }
   }
 }
