d07b1c84ff1cb02f7940b606b0f2474a61e9103f
Gaëtan Rizio <gaetan@rizio.fr>
2017-11-06 20:24:54 +0100

    Improve configuration management
    The simple user of Definiti could not care about scala-specific destination.
    Improve the configuration so it will accept `definiti.build.destination`
    as fallback configuration.
    
    Also, to avoid using multiple versions of sbt, upgrade to the last one.
    
    This commit do the following:
    
    * Destination has now a fallback to `definiti.build.destination`
    * Remove `getPathOrElse` and replace it with `getPathOpt`
    * Upgrade sbt to version 1.0.2 (last one on this day)

--- src/main/scala/definiti/scalamodel/Configuration.scala
+++ src/main/scala/definiti/scalamodel/Configuration.scala
@@ -1,6 +1,6 @@
 package definiti.scalamodel
 
-import java.nio.file.{Files, Path, Paths}
+import java.nio.file.{Path, Paths}
 
 import com.typesafe.config.{Config, ConfigFactory}
 import com.typesafe.scalalogging.Logger
@@ -9,23 +9,29 @@ private[scalamodel] class Configuration(config: Config) {
   private val logger = Logger(getClass)
 
   def this() {
-    this(ConfigFactory.load().getConfig("definiti.scalamodel"))
+    this(ConfigFactory.load())
   }
 
-  lazy val destination: Path = getPathOrElse("destination", Paths.get("target", "scalamodel"))
+  lazy val destination: Path = getFirstDefinedPath(
+    "definiti.scalamodel.destination",
+    "definiti.build.destination"
+  ).getOrElse(Paths.get("target", "scalamodel"))
 
-  private def getPathOrElse(configurationPath: String, defaultValue: => Path, mustExist: Boolean = false): Path = {
+  private def getFirstDefinedPath(keys: String*): Option[Path] = {
+    keys
+      .map(getPathOpt)
+      .collectFirst {
+        case Some(path) => path
+      }
+  }
+
+  private def getPathOpt(configurationPath: String): Option[Path] = {
     if (config.hasPath(configurationPath)) {
       val rawPath = config.getString(configurationPath)
       val path = Paths.get(rawPath)
-      if (!mustExist || Files.exists(path)) {
-        path
-      } else {
-        logger.warn(s"Path $rawPath not found, using default one.")
-        defaultValue
-      }
+      Some(path)
     } else {
-      defaultValue
+      None
     }
   }
 }
